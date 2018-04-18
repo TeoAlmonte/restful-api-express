61794c7bc72a707c25546533c18ce5348cb48d0c
Teo Almonte <teoalmonte@gmail.com>
2018-04-18 01:16:37 -0400

    endpoints and crud

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,18 +1,17 @@
-import { addNewContact } from '../controllers/Controller'
+import { addNewContact, getContacts, getContactWithId, updateContact, removeContact } from '../controllers/Controller'
 
 const routes = (app) => {
  app.route('/contact')
  .get((req, res, next) => {
     console.log(`Request from: ${req.originalUrl}`)
     console.log(`Request from: ${req.method}`)
-    res.send('GET request')
-  })
+    next()
+  }, getContacts)
  .post(addNewContact)
  app.route('/contact/:contactId')
- .put((req, res) =>
- res.send('PUT request'))
- .delete((req, res) =>
- res.send('DELETE request'))
+ .get(getContactWithId)
+ .put(updateContact)
+ .delete(removeContact)
 }
 
 export default routes
